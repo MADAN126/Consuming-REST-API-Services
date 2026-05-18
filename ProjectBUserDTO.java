b7c7c8096c5ccd7d3057d207f117cda07a5c659b
MADAN126 <madan20131312@gmail.com>
2026-05-18 22:44:09 +0530

    Create ProjectBUserDTO.java

---
+++ ProjectBUserDTO.java
@@ -0,0 +1,18 @@
+package com.ProjectB.main.ServiceLayer;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+	
+	private String email;
+	
+	private String password;
+	
+
+}
